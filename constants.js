78191326e2c18ca5ad584e587ee10c52ff8fb8cf
jakerydont <jakerydont@gmail.com>
2024-09-16 16:03:20 -0400

    constants

---
+++ constants.js
@@ -0,0 +1,11 @@
+export default {
+    twitch: {
+        sourceIcon: 'twitchIcon.png'
+    },
+    youtube: {
+        sourceIcon: 'youtubeIcon.png'
+    },
+    tiktok: {
+        sourceIcon: 'tiktokIcon.png'
+    },
+}
